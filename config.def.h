ae0312a1975d48fa71a58361a103da881f23ae4b
nephitejnf <nephitejnf@gmail.com>
2023-11-28 19:20:43 -0700

    Add autostart

--- config.def.h
+++ config.def.h
@@ -18,6 +18,14 @@ static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 1.0}; /* You can al
 
 /* logging */
 static int log_level = WLR_ERROR;
+/* Autostart */
+static const char *const autostart[] = {
+        "wbg", "/path/to/your/image", NULL,
+        NULL /* terminate */
+};
+
+/* tagging - tagcount must be no greater than 31 */
+static const int tagcount = 9;
 
 static const Rule rules[] = {
 	/* app_id     title       tags mask     isfloating   monitor */
